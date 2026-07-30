e05ff643fbd178409aa640c0d5ee29e714d8bf00
Ardian <ardiangamershort@gmail.com>
2026-07-30 15:26:56 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'abs-coder-v6-cache';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './config/abs.config.json',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
